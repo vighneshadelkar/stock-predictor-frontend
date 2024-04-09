be049d41dc8c87c6d543e9974fb604101210242f
Superfluid <goushala1@gmail.com>
2024-04-09 17:46:31 +0530

    Refactor API URL in LineChart2 component and update asset files

--- src/components/LineChart2.jsx
+++ src/components/LineChart2.jsx
@@ -14,7 +14,7 @@ export default function LineChart2() {
   useEffect(() => {
     const getPoints = async (retryCount = 0) => {
       try {
-        const response = await fetch(`http://127.0.0.1/api/stockprice/${id}/?time_step=100&future_date=30`);
+        const response = await fetch(`http://ec2-3-90-66-200.compute-1.amazonaws.com/api/stockprice/${id}/?time_step=100&future_date=30`);
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
